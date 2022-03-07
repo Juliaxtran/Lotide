8fdbbc9c3462cc3acbf425727d04cffeb75c53b0
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-07 17:02:14 -0500

    assertEqual.js
    with emojii

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+
+let emoji = String.fromCodePoint(0x1F621);
+let emoji2 = String.fromCodePoint(128525);
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(` ${emoji2}${emoji2}${emoji2}ssertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`${emoji}${emoji}${emoji}Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Favourite", "Spice");
