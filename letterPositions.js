3342158acf0cbe075b19e8f48fa809d7f39cbf24
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-09 18:50:35 -0500

    Other alternative of letter position function

--- letterPositions.js
+++ letterPositions.js
@@ -19,4 +19,19 @@ const letterPositions = function (sentence) {
 
 
 
+
+const letterPositions = function (sentence) {
+    let newSentence = sentence.toLowerCase()
+    const results = {};
+    for (let i = 0; i < newSentence.length; i++) {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+    return results;
+  };
+
+
 console.log(letterPositions("Lighthouselabs"));
