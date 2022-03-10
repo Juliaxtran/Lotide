28fc6eecd4ded23c808ec73451d22868dffda848
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-10 18:11:05 -0500

    Final Version

--- without.js
+++ without.js
@@ -1,23 +1,3 @@
-let emoji = String.fromCodePoint(0x1F621);
-let emoji2 = String.fromCodePoint(128525);
-
-
-const assertEqual = function (actual, expected) {
-    if (actual === expected) {
-        console.log(` ${emoji2}${emoji2}${emoji2}ssertion Passed: [${actual}] === [${expected}]`);
-    } else {
-        console.log(`${emoji}${emoji}${emoji}Assertion Failed: [${actual}] !== [${expected}]`);
-    }
-};
-
-const eqArrays = function (arr1, arr2) {
-    for (let i = 0; i <= arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-            console.log(`${emoji}${emoji}${emoji}The Two Array are equal : fase`);
-        }
-    }
-    console.log(`${emoji2}${emoji2}${emoji2}The Two Array are equal : true`);
-}
 
 const without = function (arr, itemsToRemove) {
     let newArr = [...arr]
