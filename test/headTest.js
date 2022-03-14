b8fc0ba5d590c5115a116f9c829894088a327bed
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-14 14:42:57 -0400

    Additional imports

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,5 @@
 const {assertEqual, emoji, emoji2} = require('../assertEqual');
-const assertEqual = require('../assertEqual');
-
+const head = require('../head');
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
