5c21e0f0ea49b6e1cb5965261e41aa13d4e7e94e
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-14 18:21:09 -0400

    Refractor form

--- flatten.js
+++ flatten.js
@@ -1,30 +1,18 @@
 
-const flatten = function(array) {
-    let flattenedArray = [];
-    for (let i = 0; i < array.length; i++) {
-        console.log("Step", i);
-      if (Array.isArray(array[i])) {
-        for (const element of array[i]) {
-          flattenedArray.push(element);
-          console.log("Step 2", flattenedArray);
-        }
-      } else {
-        flattenedArray.push(array[i]);
-        console.log("Step 3", flattenedArray);
+const flatten = function (array) {
+  let flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (const element of array[i]) {
+        flattenedArray.push(element);
       }
+    } else {
+      flattenedArray.push(array[i]);
     }
-    return flattenedArray;
-  };
-  
-console.log(flatten[[0,3,4],4,6,7,[2,4]]);
+  }
+  return flattenedArray;
+};
 
-//Psuedo Code
-// create a function flatten 
-// return an empty array 
-// the input will have nested arrays 
-// loop through array 
-// find where the array is 
-// use .isArray() ?
-// join the arrays neatly 
 
+module.exports = flatten;
 
