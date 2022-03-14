d1e54a9691ddd6aaff914b7dcfac031d39a0f84d
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-14 18:23:03 -0400

    Export form

--- takeUntil.js
+++ takeUntil.js
@@ -11,14 +11,4 @@ const takeUntil = function(array, callback) {
   }
 
 
-let pets = ["Sam","Robert","Tom", "Dave" ];
-const results1 = takeUntil(pets, pet => pet === "Tom");
-console.log(results1);
-
-const results2 = takeUntil(pets, pet => pet === "Robert");
-console.log(results2);
-
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results3 = takeUntil(data2, x => x === ',');
-console.log(results3);
+module.exports = takeUntil;
