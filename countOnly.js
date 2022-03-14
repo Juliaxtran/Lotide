6c7d0b0779a99d115a70294b690985ec356318d2
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-14 18:19:23 -0400

    Count Only Export

--- countOnly.js
+++ countOnly.js
@@ -1,14 +1,4 @@
-let emoji = String.fromCodePoint(0x1F621);
-let emoji2 = String.fromCodePoint(128525);
 
-
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(` ${emoji2}${emoji2}${emoji2}ssertion Passed: [${actual}] === [${expected}]`);
-    } else {
-      console.log(`${emoji}${emoji}${emoji}Assertion Failed: [${actual}] !== [${expected}]`);
-    }
-  };
   
   const countOnly = function(allItems, itemsToCount) {
     const results = {};
@@ -27,22 +17,5 @@ const assertEqual = function(actual, expected) {
   
 
 
-// Test Function 
-const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
-  
-  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-  
-  assertEqual(result1["Jason"], 1);
-  assertEqual(result1["Karima"], undefined);
-  assertEqual(result1["Fang"], 2);
-  assertEqual(result1["Agouhanna"], undefined);
+
+module.exports = countOnly;
