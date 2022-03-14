34622ed81d94cc10a7fc0cf2cb5ae2a32069d14f
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-14 18:18:27 -0400

    Index Refractor

--- index.js
+++ index.js
@@ -3,27 +3,29 @@ const tail   = require('./tail');
 const middle = require('./middle');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
-const findKey = ('./findKey');
-const findKeyByValue = ('./findKeyByValue')
-const flatten = ('./flatten');
-const letterPositions = ('./letterPositions');
-const map = ('./map');
-const takeUntil = ('./takeUntil');
-const without = ('./without');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey : findKey,
-  findKeyByValue: findKeyByValue,
-  flatten : flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without,
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
   
 };
+
