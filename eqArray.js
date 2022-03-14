998c0ab21a3ed84f85db81a84e00b876fba5bc6d
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-14 13:55:14 -0400

    Export form

--- eqArray.js
+++ eqArray.js
@@ -1,20 +1,4 @@
-
-let emoji = String.fromCodePoint(0x1F621);
-let emoji2 = String.fromCodePoint(128525);
-
-
-const assertEqual = function (actual, expected) {
-    if (actual === expected) {
-        console.log(` ${emoji2}${emoji2}${emoji2}ssertion Passed: [${actual}] === [${expected}]`);
-    } else {
-        console.log(`${emoji}${emoji}${emoji}Assertion Failed: [${actual}] !== [${expected}]`);
-    }
-};
-
-
-let arr1 = [1, 2, 3];
-let arr2 = [1, 2, 3];
-
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function (arr1, arr2) {
     for (let i = 0; i <= arr1.length; i++) {
@@ -25,5 +9,6 @@ const eqArrays = function (arr1, arr2) {
     return true;
 }
 
-console.log(eqArrays(arr1, arr2));
 
+
+module.exports = eqArrays;
