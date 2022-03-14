99e6a1c8c0413bcf298ba0c78ef56f750a1249dd
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-14 13:32:37 -0400

    Tail export function

--- tail.js
+++ tail.js
@@ -2,8 +2,15 @@
 const assertEqual = require('./assertEqual');
 
 
+
+
 const tail = (words) => {
+if (words.length < 1) {
+  return [];
+}
   return words.slice(1);
-};
+}
+
+module.exports = tail;
 
 module.exports = tail;
