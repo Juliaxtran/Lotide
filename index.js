828b02d39f96b0280ccc4cc4994dceb65ab8fe25
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-14 17:25:44 -0400

    Index of Lotide

---
+++ index.js
@@ -0,0 +1,29 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = ('./findKey');
+const findKeyByValue = ('./findKeyByValue')
+const flatten = ('./flatten');
+const letterPositions = ('./letterPositions');
+const map = ('./map');
+const takeUntil = ('./takeUntil');
+const without = ('./without');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey : findKey,
+  findKeyByValue: findKeyByValue,
+  flatten : flatten,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
+  
+};
