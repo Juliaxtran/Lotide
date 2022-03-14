8d7052ae406f6dfd1f503d6ca2fecd93e4c5a27d
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-14 13:33:54 -0400

    Tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -4,5 +4,19 @@ const assertEqual = require('../assertEqual');
 
 //Test Case
 const words = ["Yo Yo", "Lighthouse", "Labs"];
+const result = tail(words);
+assertEqual(result.length, 2); 
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+// Tail function Returning a new array and not modify the original that is passed 
 tail(words); 
-assertEqual(words.length, 3); 
+assertEqual(words.length, 3)
+// An array with one one element should yeild an empty array for its tail
+
+const result2 = tail(oneElem);
+assertEqual(result2,[]);
+// An empty array should yield an empty array for its tail
+const emptyArray = [];
+assertEqual(result3,[]);
+
+// Computer is detecting array as objects 
